c1447be2337847b6c78c437545799dd12079e2cc
Vince <vincivenv@gmail.com>
2022-08-11 23:08:39 +0200

    Time: 11 ms (98.28%), Space: 6.2 MB (94.34%) - LeetHub

--- 136-single-number/136-single-number.go
+++ 136-single-number/136-single-number.go
@@ -1,17 +1,7 @@
 func singleNumber(nums []int) int {
-    dict := make(map[int]int)
+    xor := 0
     for _, num := range nums {
-        if val,ok := dict[num]; ok {
-            dict[num] = val+1
-        } else {
-            dict[num] = 1
-        }
+        xor = xor^num
     }
-    var res int
-    for k, v := range dict{
-        if v == 1 {
-            res = k
-        }
-    }
-    return res
+    return xor
 }
