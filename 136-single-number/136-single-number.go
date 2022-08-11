0744a9a88b2fb43a0e96f78d7db8d2c3e2713f97
Vince <vincivenv@gmail.com>
2022-08-11 23:00:28 +0200

    Time: 23 ms (66.86%), Space: 7.2 MB (13.86%) - LeetHub

---
+++ 136-single-number/136-single-number.go
@@ -0,0 +1,17 @@
+func singleNumber(nums []int) int {
+    dict := make(map[int]int)
+    for _, num := range nums {
+        if val,ok := dict[num]; ok {
+            dict[num] = val+1
+        } else {
+            dict[num] = 1
+        }
+    }
+    var res int
+    for k, v := range dict{
+        if v == 1 {
+            res = k
+        }
+    }
+    return res
+}
