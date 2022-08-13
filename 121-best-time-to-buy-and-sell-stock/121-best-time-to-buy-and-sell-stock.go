ac30af45d311245816b5aa28299c9bb415b5356d
Vince <vincivenv@gmail.com>
2022-08-13 09:24:58 +0200

    Time: 223 ms (33.76%), Space: 9.5 MB (20.81%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.go
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.go
@@ -1,25 +1,22 @@
 func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func maxProfit(prices []int) int {
-    if len(prices) <= 1 {
-        return 0
-    }
-    left_ptr, right_ptr := 0, 1
-    max_profit := 0
-    for right_ptr < len(prices) {
-        if prices[right_ptr] > prices[left_ptr]{
-            max_profit = max(max_profit, prices[right_ptr] - prices[left_ptr])
-        } else {
-            left_ptr = right_ptr
-        }
-        right_ptr +=1
-    }
-    return max_profit
-    
-}
+	if len(prices) <= 1 {
+		return 0
+	}
+	min_price := prices[0]
+	profit := 0
+	for _, price := range prices[1:] {
+		if price < min_price {
+			min_price = price
+		}
+		profit = max(profit, price-min_price)
+	}
+	return profit
 
+}
