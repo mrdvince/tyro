296a111cc2598b473a16c52931e81fdc72916599
Vince <vincivenv@gmail.com>
2022-08-25 08:04:52 +0200

    Time: 30 ms (23.08%), Space: 7.4 MB (49.68%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.go
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.go
@@ -7,18 +7,20 @@
  * }
  */
 func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
-    if root1 == nil && root2 == nil {
-        return nil
-    }
+	// adding this line improves from 5% to 50% :)
+	if root1 == nil && root2 == nil {
+		return nil
+	}
 	if root1 == nil {
 		return root2
 	}
 	if root2 == nil {
 		return root1
 	}
-    new_tree := &TreeNode{}
-	new_tree.Val = root1.Val + root2.Val
-	new_tree.Left = mergeTrees(root1.Left, root2.Left)
-	new_tree.Right = mergeTrees(root1.Right, root2.Right)
+	new_tree := &TreeNode{
+		Val:   root1.Val + root2.Val,
+		Left:  mergeTrees(root1.Left, root2.Left),
+		Right: mergeTrees(root1.Right, root2.Right),
+	}
 	return new_tree
 }
