56c83851fea1ddc33e25eb1cd1069f1700eadea4
Vince <vincivenv@gmail.com>
2022-08-26 08:58:25 +0200

    Time: 32 ms (90.24%), Space: 9.6 MB (28.46%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.go
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.go
@@ -4,16 +4,25 @@ type NumArray struct {
 
 
 func Constructor(nums []int) NumArray {
-    return NumArray{Nums: nums}
+    cumulative := make([]int, len(nums)) 
+    // accumulate
+    // [-2,0,3,-5,2,-1] -> [-2 -2 1 -4 -2 -3]
+
+    sum := 0
+    for idx := range nums {
+        sum += nums[idx]
+        cumulative[idx] = sum
+    }    
+    return NumArray{Nums:cumulative}
 }
 
 
 func (this *NumArray) SumRange(left int, right int) int {
-    result := 0
-    for _, val := range this.Nums[left:right+1] {
-        result += val
+    // out of range guard
+    if left == 0 {
+        return this.Nums[right]
     }
-    return result
+    return this.Nums[right] - this.Nums[left-1] 
 }
 
 
