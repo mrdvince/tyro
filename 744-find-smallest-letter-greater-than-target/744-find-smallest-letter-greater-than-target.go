2b4b00fc70689756255bfc921a2c5dbd3edbee55
Vince <vincivenv@gmail.com>
2022-09-07 06:13:31 +0200

    Time: 0 ms (100.00%), Space: 2.7 MB (78.68%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.go
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.go
@@ -1,8 +1,11 @@
 func nextGreatestLetter(letters []byte, target byte) byte {
-    point := sort.Search(len(letters), func(i int) bool { return letters[i] > target })
-    if point == len(letters) {
-        return letters[0]
-    } else {
-        return letters[point]
+    for _, letter := range letters {
+        if target < letters[0] {
+            break
+        }
+        if letter > target {
+            return letter
+        }
     }
+    return letters[0]
 }
