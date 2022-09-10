fc86d648b1a314a9667772925bff6ac20e80f099
Vince <vincivenv@gmail.com>
2022-09-10 23:29:30 +0200

    Time: 206 ms (89.51%), Space: 19 MB (82.14%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.go
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.go
@@ -10,35 +10,24 @@ func minDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-    left := minDepth(root.Left)
-    right := minDepth(root.Right)
-    if left == 0 {
-        return right + 1
-    }
-    if right == 0 || left < right {
-        return left + 1
-    }
-    return right + 1
-}
-// 	depth, queue := 0, []*TreeNode{root}
+	depth, queue := 1, []*TreeNode{root}
 
-// 	for len(queue) > 0 {
-//         tmp := []*TreeNode{}
-//         depth += 1
-// 		for _, curr_node := range queue {
-// 			// are we at a leaf node?
-// 			if curr_node.Left == nil && curr_node.Right == nil {
-                
-// 				return depth
-// 			}
-// 			if curr_node.Left != nil {
-// 				tmp = append(tmp, curr_node.Left)
-// 			}
-// 			if curr_node.Right != nil {
-// 				tmp = append(tmp, curr_node.Right)
-// 			}
-// 		}
-// 		queue = tmp
-// 	}
-// 	return depth
-// }
+	for  {
+        tmp := []*TreeNode{}
+		for _, curr_node := range queue {
+			// are we at a leaf node?
+			if curr_node.Left == nil && curr_node.Right == nil {
+				return depth
+			}
+			if curr_node.Left != nil {
+				tmp = append(tmp, curr_node.Left)
+			}
+			if curr_node.Right != nil {
+				tmp = append(tmp, curr_node.Right)
+			}
+		}
+        depth += 1
+		queue = tmp
+	}
+	return depth
+}
