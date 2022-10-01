363ab017a9e972b52c9e4c0f52dfc46edf8fb29c
Vince <vincivenv@gmail.com>
2022-10-01 15:29:51 +0200

    Time: 15 ms (47.98%), Space: 2.9 MB (82.11%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.go
@@ -0,0 +1,21 @@
+func romanToInt(s string) int {
+    numerals := map[byte]int{
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000,
+    }
+    result := 0
+    for idx := 0; idx < len(s); idx ++ {
+        if idx + 1 < len(s) && numerals[s[idx]] < numerals[s[idx+1]] {
+            result = result - numerals[s[idx]]
+        } else {
+            result += numerals[s[idx]]
+        }
+    } 
+    return result
+    
+}
