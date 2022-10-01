b95e781298a285408bcb24d04a3ddcb97bd27529
Vince <vincivenv@gmail.com>
2022-10-01 15:43:26 +0200

    Time: 7 ms (88.93%), Space: 2.8 MB (100.00%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.go
+++ 13-roman-to-integer/13-roman-to-integer.go
@@ -1,19 +1,19 @@
 func romanToInt(s string) int {
-    numerals := map[string]int{
-        "I":1,
-        "V":5,
-        "X":10,
-        "L":50,
-        "C":100,
-        "D":500,
-        "M":1000,
+    numerals := map[byte]int{
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000,
     }
     result := 0
     for idx := 0; idx < len(s); idx ++ {
-        if idx + 1 < len(s) && numerals[string(s[idx])] < numerals[string(s[idx+1])] {
-            result -= numerals[string(s[idx])]
+        if idx + 1 < len(s) && numerals[s[idx]] < numerals[s[idx+1]] {
+            result -= numerals[s[idx]]
         } else {
-            result += numerals[string(s[idx])]
+            result += numerals[s[idx]]
         }
     } 
     return result
