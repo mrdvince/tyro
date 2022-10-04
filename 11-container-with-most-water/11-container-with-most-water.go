b57a66d6ed8596ebb2ff8511a20619770497578d
Vince <vincivenv@gmail.com>
2022-10-04 20:20:31 +0200

    Time: 185 ms (30.42%), Space: 9 MB (54.69%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.go
+++ 11-container-with-most-water/11-container-with-most-water.go
@@ -1,6 +1,8 @@
 func maxArea(height []int) int {
     maxArea, leftPtr, rightPtr := 0, 0, len(height) - 1
-    
+    if len(height) <=2 {
+        return min(height[0], height[1])
+    }
     for leftPtr < rightPtr {        
         maxArea = max(maxArea, min(height[leftPtr], height[rightPtr]) * (rightPtr - leftPtr))
         if height[leftPtr] < height[rightPtr] {
