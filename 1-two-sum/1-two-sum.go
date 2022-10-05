5c9c42cb7550051b785cfc2d7bb4feda8d1e6c83
Vince <vincivenv@gmail.com>
2022-10-05 23:41:09 +0200

    Time: 4 ms (96.24%), Space: 4.2 MB (56.56%) - LeetHub

--- 1-two-sum/1-two-sum.go
+++ 1-two-sum/1-two-sum.go
@@ -1,17 +1,11 @@
 func twoSum(nums []int, target int) []int {
-    values := []int{}
     dict := map[int]int{}
-    
     for idx, num := range nums {
         missingNum := target - num
-        
         if val, ok := dict[missingNum]; ok {
             return []int{idx,val} 
         }
         dict[num] = idx
     }
-    
-    return values
-    
-    
+    return []int{}
 }
