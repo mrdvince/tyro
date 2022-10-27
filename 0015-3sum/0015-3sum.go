d64bbb011500489b25876441e4f6c2b4d996bfd7
Vince <vincivenv@gmail.com>
2022-10-27 13:07:17 +0200

    Time: 48 ms (72.04%), Space: 7.7 MB (42.83%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -1,29 +1,29 @@
 func threeSum(nums []int) [][]int {
-    res := [][]int{}
-    sort.Slice(nums, func(i,j int) bool{ return nums[i] < nums[j] })
-    
-    for idx, num := range nums {
-        if idx > 0 && num == nums[idx-1] {
-            continue
-        }
+	res := [][]int{}
+	sort.Slice(nums, func(i, j int) bool { return nums[i] < nums[j] })
 
-        leftPtr, rightPtr := idx + 1, len(nums) - 1
-        for leftPtr < rightPtr {
-            threeSum := num + nums[leftPtr] + nums[rightPtr]
-            
-            if threeSum > 0 {
-                rightPtr -= 1
-            } else if threeSum < 0 {
-                leftPtr += 1
-            } else {
-                res = append(res, []int{num, nums[leftPtr], nums[rightPtr]})
-                leftPtr += 1
-                for nums[leftPtr] == nums[leftPtr - 1] && leftPtr < rightPtr {
-                    leftPtr += 1
-                }
-            }
-        }
-    }
-    
-    return res
+	for idx, num := range nums {
+		if idx > 0 && num == nums[idx-1] {
+			continue
+		}
+
+		leftPtr, rightPtr := idx+1, len(nums)-1
+		for leftPtr < rightPtr {
+			threeSum := num + nums[leftPtr] + nums[rightPtr]
+
+			if threeSum > 0 {
+				rightPtr -= 1
+			} else if threeSum < 0 {
+				leftPtr += 1
+			} else {
+				res = append(res, []int{num, nums[leftPtr], nums[rightPtr]})
+				leftPtr += 1
+				for nums[leftPtr] == nums[leftPtr-1] && leftPtr < rightPtr {
+					leftPtr += 1
+				}
+			}
+		}
+	}
+
+	return res
 }
