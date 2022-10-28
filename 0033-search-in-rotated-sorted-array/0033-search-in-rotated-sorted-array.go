d6520c8b59c781a0d4b366c0aee7df80e540df6e
Vince <vincivenv@gmail.com>
2022-10-29 00:01:57 +0200

    Time: 4 ms (67.70%), Space: 2.5 MB (100.00%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
@@ -1,8 +1,28 @@
 func search(nums []int, target int) int {
-    for idx, num := range nums {
-        if num == target {
-            return idx
+    leftPtr, rightPtr := 0, len(nums) - 1
+    
+    for leftPtr <= rightPtr {
+        mid := (leftPtr + rightPtr) // 2
+        if nums[mid] == target {
+            return mid
+        }
+        mid_num := nums[mid]
+        if nums[leftPtr] <= mid_num {
+            if target < nums[leftPtr] || target > mid_num {
+                // got right
+                leftPtr = mid + 1
+            } else {
+                rightPtr = mid - 1
+            }
+        } else {
+            if target > nums[rightPtr] || target < mid_num {
+                // go left
+                rightPtr = mid - 1
+            } else {
+                leftPtr = mid + 1
+            }
         }
     }
+    
     return -1
 }
