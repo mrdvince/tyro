669ef9e02600390680727ad75cb86b039f444e71
Vince <vincivenv@gmail.com>
2022-10-31 23:48:06 +0100

    Time: 4 ms (39.82%), Space: 2.3 MB (72.11%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.go
@@ -0,0 +1,21 @@
+func rotate(matrix [][]int)  {
+    left, right := 0, len(matrix[0]) - 1
+    
+    for left < right {
+        for idx := 0; idx < (right - left); idx ++ {
+            top, bottom := left, right
+            
+            topLeft := matrix[top][left + idx]
+            
+            matrix[top][left + idx] = matrix[bottom - idx][left]
+            
+            matrix[bottom - idx][left] = matrix[bottom][right - idx]
+            
+            matrix[bottom][right - idx] = matrix[top + idx][right]
+            
+            matrix[top + idx][right] = topLeft
+        }
+        left += 1
+        right -= 1
+    }
+}
