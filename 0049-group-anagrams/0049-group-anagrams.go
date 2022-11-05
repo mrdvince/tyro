d30d89da298412bac0cda8502c4d3cc6841f72b2
Vince <vincivenv@gmail.com>
2022-11-05 12:51:03 +0100

    Time: 41 ms (60.01%), Space: 8.2 MB (68.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.go
+++ 0049-group-anagrams/0049-group-anagrams.go
@@ -1,20 +1,17 @@
 func groupAnagrams(strs []string) [][]string {
-	res := map[string][]string{}
-
+	res := map[[26]int][]string{}
 	for _, s := range strs {
-		count := make([]int, 26)
+        count := [26]int{}
 		for _, c := range s {
 			count[c-'a'] += 1
 		}
-		key := strings.Trim(fmt.Sprint(count), "[]")
-		res[key] = append(res[key], s)
+		res[count] = append(res[count], s)
 	}
 
 	values := make([][]string, 0, len(res))
 	for _, v := range res {
 		values = append(values, v)
 	}
-	fmt.Println(values)
 
 	return values
 }
