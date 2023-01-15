d10ad2fa7ad4884987b64c62bd479fe7ed809928
Vince <vincivenv@gmail.com>
2023-01-15 23:41:49 +0100

    Time: 399 ms (99.28%), Space: 69.8 MB (38.13%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.go
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.go
@@ -1,41 +1,40 @@
 type TimeMap struct {
-    KvStore map[string][]KVPair
+	KvStore map[string][]KVPair
 }
 
 type KVPair struct {
-    value string
-    timestamp int
+	value     string
+	timestamp int
 }
 
 func Constructor() TimeMap {
-    return TimeMap{}
+	return TimeMap{
+		KvStore: make(map[string][]KVPair),
+	}
 }
 
-func (this *TimeMap) Set(key string, value string, timestamp int)  {
-    if this.KvStore == nil {
-        this.KvStore = make(map[string][]KVPair)
-    }
-    this.KvStore[key] = append(this.KvStore[key], KVPair{value, timestamp})
+func (this *TimeMap) Set(key string, value string, timestamp int) {
+	this.KvStore[key] = append(this.KvStore[key], KVPair{value, timestamp})
 }
 
 func (this *TimeMap) Get(key string, timestamp int) string {
-    values, ok := this.KvStore[key]
-    if !ok {
-        return ""
-    }
-    l, r := 0, len(values) -1
-    for l <= r {
-        m := (l + r) / 2
-        if values[m].timestamp <= timestamp {
-            if m == len(values) -1 || values[m+1].timestamp > timestamp {
-                return values[m].value
-            }
-            l = m + 1
-        } else {
-            r = m -1
-        }
-    }
-    return ""
+	values, ok := this.KvStore[key]
+	if !ok {
+		return ""
+	}
+	l, r := 0, len(values)-1
+	for l <= r {
+		m := l + (r-l)/2
+		if values[m].timestamp <= timestamp {
+			if m == len(values)-1 || values[m+1].timestamp > timestamp {
+				return values[m].value
+			}
+			l = m + 1
+		} else {
+			r = m - 1
+		}
+	}
+	return ""
 }
 
 
