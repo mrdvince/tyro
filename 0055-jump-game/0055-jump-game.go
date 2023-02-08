03f2ee70520a592844c1568be4db478d0bdaaac1
Vince <vincivenv@gmail.com>
2023-02-08 21:02:51 +0100

    Time: 57 ms (86.09%), Space: 6.9 MB (87.04%) - LeetHub

--- 0055-jump-game/0055-jump-game.go
+++ 0055-jump-game/0055-jump-game.go
@@ -1,13 +1,14 @@
 func canJump(nums []int) bool {
-    end := len(nums) - 1
-    for idx := len(nums) -1; idx >= 0; idx-- {
-        if idx + nums[idx] >= end {
-            end = idx
-        }
-    }
-    if end == 0 {
-        return true
-    } else {
-        return false
-    }
+	nos := len(nums)
+	if nos == 0 {
+		return false
+	}
+
+	lastPos := nos - 1
+	for idx := nos - 1; idx >= 0; idx-- {
+		if idx+nums[idx] >= lastPos {
+			lastPos = idx
+		}
+	}
+	return lastPos == 0
 }
